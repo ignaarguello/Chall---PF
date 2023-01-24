c8bd3010a8a5e32e6dce3788df1e0f9215f92368
ignaarguello <igna.arguello2110@hotmail.com>
2023-01-24 16:02:29 -0300

    Estilos + aos animated

--- src/componentes/Header/Header.jsx
+++ src/componentes/Header/Header.jsx
@@ -5,11 +5,11 @@ import './Header.css'
 export default function Header() {
   return (
     <div id='container-general__header'>
-      <h2>Challenge - ProFlight</h2>
-      <p>- Creamos un componente tal cual como se pedia en la consigna.
+      <h2 id='titulo-challenge__header'>Challenge - ProFlight</h2>
+      <p className='texto-description__header'>- Creamos un componente tal cual como se pedia en la consigna.
         Cuando damos click pasa la 'propiedad de css' al componente siguiente o anterior.
       </p>
-      <p>- Gracias a los eventos aplicados en las flechas, logramos interactuar con la interfaz.</p>
+      <p className='texto-description__header'>- Gracias a los eventos aplicados en las flechas, logramos interactuar con la interfaz.</p>
     </div>
   )
 }
