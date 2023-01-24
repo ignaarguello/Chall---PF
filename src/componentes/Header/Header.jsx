95a83650edaf1742d7f71134de5732e5714315fd
ignaarguello <igna.arguello2110@hotmail.com>
2023-01-24 16:16:04 -0300

    Ultimo commit

--- src/componentes/Header/Header.jsx
+++ src/componentes/Header/Header.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import './Header.css'
+import 'animate.css';
 
 
 export default function Header() {
   return (
     <div id='container-general__header'>
-      <h2 id='titulo-challenge__header'>Challenge - ProFlight</h2>
-      <p className='texto-description__header'>- Creamos un componente tal cual como se pedia en la consigna.
+      <h2 id='titulo-challenge__header' className='animate__animated animate__lightSpeedInLeft animate__slow'>Challenge - ProFlight</h2>
+      <p className='texto-description__header animate__animated animate__fadeIn animate__delay-1s'>- Creamos un componente tal cual como se pedia en la consigna.
         Cuando damos click pasa la 'propiedad de css' al componente siguiente o anterior.
       </p>
-      <p className='texto-description__header'>- Gracias a los eventos aplicados en las flechas, logramos interactuar con la interfaz.</p>
+      <p className='texto-description__header animate__animated animate__fadeIn animate__delay-2s'>- Gracias a los eventos aplicados en las flechas, logramos interactuar con la interfaz.</p>
     </div>
   )
 }
